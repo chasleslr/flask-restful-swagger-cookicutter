d0882aaa3d09e2acde82e8bc9161ae29cb509bcb
chasleslr <charles@dribbble.com>
2021-06-08 09:32:08 -0400

    fix relative imports

--- {{cookiecutter.repo_name}}/{{ cookiecutter.project_slug }}/api/resources.py
+++ {{cookiecutter.repo_name}}/{{ cookiecutter.project_slug }}/api/resources.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from flask_apispec import MethodResource, marshal_with, doc
 
-from ..serializers.healthcheck import HealthcheckResponseSchema
+from .serializers.healthcheck import HealthcheckResponseSchema
 
 
 class HealthcheckResource(Resource, MethodResource):
