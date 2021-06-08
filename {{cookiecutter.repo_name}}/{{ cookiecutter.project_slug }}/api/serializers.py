9cb922e7a82b28bb5bb55c4c042a86fae263fdbc
chasleslr <charles@dribbble.com>
2021-06-08 09:32:00 -0400

    mode error handler into serializers

--- {{cookiecutter.repo_name}}/{{ cookiecutter.project_slug }}/api/serializers.py
+++ {{cookiecutter.repo_name}}/{{ cookiecutter.project_slug }}/api/serializers.py
@@ -1,5 +1,15 @@
 from marshmallow import Schema, fields
+from webargs.flaskparser import abort, parser
 
 
 class HealthcheckResponseSchema(Schema):
     status = fields.String()
+
+
+@parser.error_handler
+def handle_request_parsing_error(err, req, schema, error_status_code, error_headers):
+    """webargs error handler that uses Flask-RESTful's abort function to return
+    a JSON error response to the client.
+    """
+    status_code = error_status_code or 422
+    abort(status_code, errors=err.messages)
